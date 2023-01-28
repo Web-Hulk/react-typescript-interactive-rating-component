dc9e02eb32e77d20090ad1f687396af6cea2e68d
IJ58LG <patryk.kwasek@ing.com>
2023-01-28 23:53:02 +0100

    Initialized project + added HTML and stylings to Rating Card Component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,33 @@
+import iconStar from "../../assets/icon-star.svg";
+import "./App.scss";
+
+function App() {
+  return (
+    <main className="main">
+      <div className="container">
+        <div className="star-container">
+          <img src={iconStar} alt="Icon star" className="icon-star-image" />
+        </div>
+
+        <h1 className="title">How did we do?</h1>
+
+        <p className="description">
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve our offering!
+        </p>
+
+        <div className="rate-buttons-container">
+          <button className="rate-btn">1</button>
+          <button className="rate-btn">2</button>
+          <button className="rate-btn">3</button>
+          <button className="rate-btn">4</button>
+          <button className="rate-btn">5</button>
+        </div>
+
+        <button className="submit-btn">Submit</button>
+      </div>
+    </main>
+  );
+}
+
+export default App;
